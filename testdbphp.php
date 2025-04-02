98f190c5e98892924bd89a16bf57a69a03209e7d
nick <znickmeyer@gmail.com>
2025-04-02 00:27:39 +0000

    Updated Findemployee and testdbphp

--- testdbphp.php
+++ testdbphp.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Northland Analytics PHP/MySQL Test Page</title>
+    <title>Northland Analytics Employee Management System</title>
 </head>
 <body>
     <h1>Northland Analytics PHP/MySQL Test Page</h1>
